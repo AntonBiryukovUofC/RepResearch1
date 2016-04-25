2073c9c2b5c264d7dcde173f3e68a4a04ab7e0fd
anton.biryukov@ucalgary.ca <anton.biryukov@ucalgary.ca>
2016-04-25 17:53:03 -0400

    sleepy a.f. #3

--- Script1.R
+++ Script1.R
@@ -31,15 +31,53 @@ activity_list_2 <- activity_list %>% filter(!is.na(steps)) %>%  # Filter the NAs
   group_by(interval) %>% # Group by the interval
   summarize(StepsPerInt = mean(steps)) # Sum of the steps elements in each group
 
-# Before imputing values
+# Before imputing values,
 p1 <- qplot(StepsPerDay,data = activity_list_1,geom = "histogram",bins = 20,main="Histogram of total steps per day")
 Mean_Median <-as.data.frame(t(c(mean(activity_list_1$StepsPerDay),median(activity_list_1$StepsPerDay))))
 colnames(Mean_Median) <- c("Mean of steps per day","Median of steps per day")
 p2 <- qplot(interval,StepsPerInt,data = activity_list_2,geom="line",main=" Average # steps per interval")
-grid.arrange(p1,p2,ncol=2)
 print(Mean_Median)
+grid.arrange(p1,p2,ncol=2)
+
 # Now impute the values: I will use the mean for the corresponding interval:
-activity_list_3 <- activity_list %>% mutate(stepsImp = imputeNA(steps,interval,activity_list_2))
+activity_list_3 <- activity_list %>% mutate(steps = imputeNA(steps,interval,activity_list_2))
+grouped_imputed  <- activity_list_3 %>% 
+  group_by(date) %>% # Group by the date
+  summarize(StepsPerDay = sum(steps)) # Sum of the steps elements in each group
+p3 <- qplot(StepsPerDay,data = grouped_imputed,geom = "histogram",bins = 20,main="Histogram of total steps per day")
+
+grid.arrange(p1,p3,ncol=2)
+
+# Calculate the mean and the median numbers to see the change:
+Mean_Median_Im <-as.data.frame(t(c(mean(grouped_imputed$StepsPerDay),median(grouped_imputed$StepsPerDay))))
+colnames(Mean_Median_Im) <- c("(Imputed) Mean of steps per day","Median of steps per day")
+print(Mean_Median_Im)
+# Now use the imputed dataset to analyze the changes in the activity pattern between weekdays and weekends
+# Make a isweekend function:
+is.weekend <- function(x)
+{
+  if (x %in% c("Sat","Sun"))
+  {
+    y = "Weekend"
+  }
+  else {y = "Weekday"}
+}
+# Augment the imputed dataset with the is-week-end flags.
+grouped_by_weekday <- activity_list_3 %>% mutate(isweekend = as.factor(sapply(wday(date,label=TRUE),is.weekend))) %>%
+                    group_by(interval,isweekend) %>% summarize(StepsPerInt = mean(steps)) 
+p4 <- qplot(interval,StepsPerInt,data = grouped_by_weekday,facets = isweekend~.,colour = isweekend)
+print(p4)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
