a2dbe3b36e6d005400a3616919bd0be122892bff
anton.biryukov@ucalgary.ca <anton.biryukov@ucalgary.ca>
2016-04-27 16:47:59 -0400

    no need to randomize, the data at the end is bullshit

--- NOAA_analysis.R
+++ NOAA_analysis.R
@@ -2,9 +2,11 @@
 library("dplyr")
 library("lubridate")
 library("ggplot2")
-library("sqldf")
-library(R.utils)
-dir.create("./Data/")
+library("R.utils")
+if (!file.exists("./Data"))
+{
+    dir.create("./Data/")
+}
 # Check whether the data file is present in the folder:
 if (!file.exists("./Data/StormData.csv.bz2"))
 {
@@ -16,19 +18,25 @@ if (!file.exists("./Data/StormData.csv.bz2"))
 # Step 1: read the data
 # Will read a subset of the Big Storm dataset (~10%):
 work_on_subset <- TRUE
-
+n_row <-  20000
 # If the data is not in the memory yet:
 if (!exists("stormData"))
 {
-    bunzip2("./Data/StormData.csv.bz2", "./Data/Storm_dataset.csv", remove = FALSE, skip = TRUE)
-    # Get the header, put it in the subset:
-    system("head -1 ./Data/Storm_dataset.csv > ./Data/Storm_subset.csv")
-    system("perl -ne 'print if (rand() < 0.10)' ./Data/Storm_dataset.csv > ./Data/Storm_subset.csv")
+    # Unzip the data
+    if (!file.exists("./Data/Storm_dataset.csv")){
+        print("unzipping the data")
+        bunzip2("./Data/StormData.csv.bz2", "./Data/Storm_dataset.csv", remove = FALSE, skip = TRUE)
+        print("Done unzipping!")
+        
+    }
+    
+    # Get the header, put it in the subset later:
+    print("Forming a subset of data")
     if (work_on_subset){
-        stormData <- read.csv("Storm_subset.csv")    
+        stormData <- read.csv("./Data/Storm_dataset.csv",nrows = n_row)
     }
     else{
-        stormData <- read.csv("Storm_dataset.csv")
+        stormData <- read.csv("./Data/Storm_dataset.csv")
     }
     
 }
